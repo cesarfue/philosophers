c8776054022a7926b7cfc0f9c4e089111edf1646
cefuente <cfuen55010@gmail.com>
2024-03-05 13:33:18 +0100

    .

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 11:24:53 by cesar             #+#    #+#             */
-/*   Updated: 2024/03/05 11:26:05 by cesar            ###   ########.fr       */
+/*   Updated: 2024/03/05 12:51:40 by cefuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+# include "stdio.h"
+# include "stdlib.h"
+# include "unistd.h"
+# include "pthread.h"
+
 
 #endif 
